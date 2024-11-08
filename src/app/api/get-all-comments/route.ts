a8cbdc55ecb424a25d06ae54ddfb42272c921e57
Adrian <adriananicic@gmail.com>
2024-11-08 14:46:30 +0100

    cache control

--- src/app/api/get-all-comments/route.ts
+++ src/app/api/get-all-comments/route.ts
@@ -4,11 +4,36 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const comments = await prisma.comment.findMany();
-    return NextResponse.json({ success: true, comments });
+
+    return NextResponse.json(
+      { success: true, comments },
+      {
+        status: 200,
+        headers: {
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
+          Pragma: "no-cache",
+          Expires: "0",
+          "Surrogate-Control": "no-store",
+        },
+      }
+    );
   } catch (error) {
-    return NextResponse.json({
-      success: false,
-      error: (error as Error).message,
-    });
+    return NextResponse.json(
+      {
+        success: false,
+        error: (error as Error).message,
+      },
+      {
+        status: 500,
+        headers: {
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
+          Pragma: "no-cache",
+          Expires: "0",
+          "Surrogate-Control": "no-store",
+        },
+      }
+    );
   }
 }
